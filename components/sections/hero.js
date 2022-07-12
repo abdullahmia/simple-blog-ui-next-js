f54cbd2c1c54ef70890176428bbbf091041571b3
Abdullah Mia <abdullah149161@bsdi-bd.org>
2022-07-12 19:39:55 +0600

    :lipstick: [Hero section Ui]

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -1,9 +1,11 @@
+import Slide from '../custom/slide'
 
 const hero = () => {
   return (
     <section className="py-16">
         <div className="container mx-auto md:px-20">
-            <h2>trending</h2>
+            <h2 className="font-bold text-4xl pb-12 capitalize text-center">trending</h2>
+            <Slide />
         </div>
     </section>
   )
